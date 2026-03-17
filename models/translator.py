1414c78d12b7cdb1cd8b071f8a10e31ec58f7881
aknidhi <amannidhi45@gmail.com>
2026-03-17 23:34:14 +0530

    janvoice-ai

--- models/translator.py
+++ models/translator.py
@@ -1,9 +1,14 @@
-from googletrans import Translator
-
-translator = Translator()
+from deep_translator import GoogleTranslator
 
 def translate_text(text):
+
     try:
-        return translator.translate(text, dest="en").text
-    except:
+        translated = GoogleTranslator(
+            source="auto",
+            target="en"
+        ).translate(text)
+
+        return translated
+
+    except Exception:
         return text
